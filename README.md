880b69feea9fa63eee4f5fc922ee23951cde1683
Zac Siegel <zac.siegel89@gmail.com>
2022-05-25 10:58:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#### Color generator
+
+An app made using React that accepts a hex color value and returns varying shades and tints of that color. Uses the values.js library for obtaining color values.
+
+[values.js](https://github.com/noeldelgado/values.js)
+
+Demo can be seen at: https://extraordinary-gingersnap-5148d6.netlify.app/
---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#7ef1c5").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      setError(false);
+    } catch (error) {
+      setError(true);
+      alert("Please enter a valid hex color value.");
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="color"
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#7ef1c5"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
